97fd993dfc307ddb4cd5793222d412782b64724f
eunbii <dmsql7142>
2018-03-31 15:57:08 +0900

    sd

---
+++ src/main/java/beak/tetromino_14500/tetro_Main.java
@@ -0,0 +1,23 @@
+package beak.tetromino_14500;
+
+/**
+ * Created by eunbi on 2018-01-12.
+ */
+
+public class tetro_Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+    public static java.util.Scanner sc = new java.util.Scanner(System.in);
+    public static int C;
+    public static int N;
+    public static int M;
+    public static void main(String[] args)throws Exception {
+        solve();
+
+    }
+
+    public static void solve()throws Exception {
+        System.out.println("HI");
+    }
+}
+
